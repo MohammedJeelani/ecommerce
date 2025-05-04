7205925c5be24e3f45a7ca2f53faca505b2396b3
Mohammed Jeelani <jilany1925@gmail.com>
2025-05-03 23:51:07 -0700

    folder strcuture created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'product/:id', loadChildren: () => import('./features/product/product.module').then(m => m.ProductModule) },
+  { path: 'cart', loadChildren: () => import('./features/cart/cart.module').then(m => m.CartModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
